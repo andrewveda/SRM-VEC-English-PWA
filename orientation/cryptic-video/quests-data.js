36be19a431cc86a0242c1e641fdcde5e30fdc9fb
Andrew Veda <andrewveda@gmail.com>
2026-08-27 09:24:04 +0530

    Create quests-data.js

---
+++ orientation/cryptic-video/quests-data.js
@@ -0,0 +1,41 @@
+const TESTS = [
+  {
+    id: "cryptic 1", questId: "Quest Video Cryptic 1", category: "Cryptic", difficulty: "Long",
+    title: "The DEFINITIVE Guide To Cryptic Crosswords", youtubeId: "0T3HOfHmetQ", durationHint: 2721,
+
+    description: "This guide should give a simple introduction to solve Cryptic Crosswords!",
+    questions: [
+{ at: 10, tier: 1,
+      q: "What do cryptic crosswords train you to become?",
+options: [
+  "A. I am a genius! I love these puzzles!",
+  "B. I am a genius! I am training my creative critical thinking!",
+  "C. I am a genius! I am going to create cryptic puzzles!",
+  "D. I am a genius! All of the above!"
+],
+correct: 3 }
+    ]
+  },
+  { at: 11, tier: 2,
+      q: "What do cryptic crosswords train you to become?",
+options: [
+  "A. I am a genius! I love these puzzles!",
+  "B. I am a genius! I am training my creative critical thinking!",
+  "C. I am a genius! I am going to create cryptic puzzles!",
+  "D. I am a genius! All of the above!"
+],
+correct: 3 }
+    ]
+  },
+    { at: 12, tier: 3,
+      q: "What do cryptic crosswords train you to become?",
+options: [
+  "A. I am a genius! I love these puzzles!",
+  "B. I am a genius! I am training my creative critical thinking!",
+  "C. I am a genius! I am going to create cryptic puzzles!",
+  "D. I am a genius! All of the above!"
+],
+correct: 3 }
+    ]
+  },
+];
